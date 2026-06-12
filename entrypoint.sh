42c437d1ea55109949de5048ebcd6c2132db9b3a
Yagouan <generaia@proton.me>
2026-06-13 00:09:29 +0200

    fix: hardcode SUPABASE_URL=/api, simplify envsubst

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Replace SUPABASE_ANON_KEY placeholder with the actual env var
-envsubst '${SUPABASE_ANON_KEY}' < /usr/share/nginx/html/index.html.template > /usr/share/nginx/html/index.html
+# Replace env vars in template
+envsubst '${SUPABASE_ANON_KEY} ${SUPABASE_URL}' < /usr/share/nginx/html/index.html.template > /usr/share/nginx/html/index.html
 # Start nginx
 nginx -g 'daemon off;'
